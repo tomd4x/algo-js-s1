b530ffb1b204edf9fec557ddd3726e841ced1f5b
tomd4x <146094322+tomd4x@users.noreply.github.com>
2023-09-26 17:46:10 +0200

    Adding learning exercice 1 EXERCICE 2

---
+++ exercice-2/main.js
@@ -0,0 +1,13 @@
+var a=4
+var b=4
+var c=3
+switch(a){
+    case b:
+        console.log('egal a b')
+    break
+    case c:
+         console.log('egal a c')
+    break
+    default:
+        console.log('egal a rien')
+}    
---
+++ learning/main.js
@@ -0,0 +1,3 @@
+alert("tu veux vraiment quitter ce site ?")
+console.log("bienvenue sur la console")
+var cardName="choipeau"
