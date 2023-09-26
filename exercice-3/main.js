a688c6117c9ca0be1d14a0f5f4bc521cbc10f764
tomd4x <146094322+tomd4x@users.noreply.github.com>
2023-09-26 18:53:00 +0200

    adding exo 3 et 4

---
+++ exercice-3/main.js
@@ -0,0 +1,4 @@
+var a=11
+for(let i=0;a>i;i++){
+    console.log("oklm")
+}
---
+++ exercice-4/main.js
@@ -0,0 +1,11 @@
+let a=3
+while(a<9){
+    a++
+    if(a==7){
+        continue
+    }
+    if(a=8){
+      break
+    }
+    console.log(a)
+}
